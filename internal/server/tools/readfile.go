30913be89f417a3a29e6345ea33175fea3589382
Kartik <kartikkannan22@gmail.com>
2026-04-14 15:44:43 +0530

    hide credentials in .env

--- internal/server/tools/readfile.go
+++ internal/server/tools/readfile.go
@@ -2,9 +2,32 @@ package tools
 
 import (
 	"os"
-	"path/filepath"
 	"strings"
 )
+
+func hideCreds(data string) string {
+	safe_data := ""
+	lines := strings.Split(data, "\n")
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		s := strings.SplitN(line, "=", 2)
+		if len(s) < 2 {
+			safe_data += line + "\n"
+		}
+		key, val := s[0], s[1]
+
+		if len(val) <= 4 {
+			safe_data += key + "=" + val + "\n"
+			continue
+		}
+
+		masked := strings.Repeat("*", len(val)-4) + val[len(val)-4:]
+		safe_data += key + "=" + masked + "\n"
+	}
+	return safe_data
+}
 
 func ReadFile(ctx ToolContext, args map[string]any) (ToolResponse, error) {
 	path, ok := args["path"].(string)
@@ -24,18 +47,22 @@ func ReadFile(ctx ToolContext, args map[string]any) (ToolResponse, error) {
 		return ToolResponse{Content: "Error: " + err.Error()}, nil
 	}
 	path = resolved
-	gitignore, err := os.ReadFile(filepath.Join(ctx.WorkingDirectory, ".gitignore"))
-	if err == nil {
-		files_to_ignore := strings.Split(string(gitignore), "\n")
-		for _, file := range files_to_ignore {
-			if strings.HasSuffix(path, file) {
-				return ToolResponse{Content: "Error: File is in .gitignore"}, nil
-			}
-		}
-	}
+	// gitignore, err := os.ReadFile(filepath.Join(ctx.WorkingDirectory, ".gitignore"))
+	// if err == nil {
+	// 	files_to_ignore := strings.Split(string(gitignore), "\n")
+	// 	for _, file := range files_to_ignore {
+	// 		if strings.HasSuffix(path, file) {
+	// 			return ToolResponse{Content: "Error: File is in .gitignore"}, nil
+	// 		}
+	// 	}
+	// }
 	data, err := os.ReadFile(path)
 	if err != nil {
 		return ToolResponse{Content: "Error: " + err.Error()}, err
+	}
+	if strings.HasSuffix(path, ".env") {
+		safeData := hideCreds(string(data))
+		return ToolResponse{Content: safeData}, nil
 	}
 	content := string(data)
 	lines := strings.Split(content, "\n")
